1feb2cf6dafd4baf19219fa22e8d5fdc5ebfa50a
uczen <Hannover1992@googlemail.com>
2021-08-26 20:55:58 +0200

    Make multiple funktion that change the content of main frame with custom text, and add youtube video with my recent project

--- script.js
+++ script.js
@@ -19,5 +19,14 @@ function odliczanie()
 
 function wer_bin_ich() {
 	var elem = document.getElementById("head_text");
-	elem.innerHTML = "3new_html"
-}
+	elem.innerHTML = "Ich bin ein technische Informatiker.Jemand der sowohl Informatik als auch Elektrotechnik in Bezug auf Rechnergestützte Anwendung studiert hat.Dank meinem Studium habe ich ein sehr tiefes und grundlegendes Verständnis für die aufbaut und Funktionsweise eines Computers.\n" }
+
+function was_kann_ich() {
+	var elem = document.getElementById("head_text");
+	elem.innerHTML = " Ich kann sowohl in das Maschinennahe C/C++ als auch in Objekt Orientiertes Java programmieren. Wenn sie eine Datenbank benötigen, kann ich es von Relationales Algebra bis hin zu deklarative  SQL aufbauen, erweitern oder \npflegen. Des Weiteren kenne ich mich mit HTML/CSS und PHP, somit kann ich für ihnen eine einfache Werbung bis hin zu  einem Online-Shop entwerfen." }
+
+function contackt() {
+	var elem = document.getElementById("head_text");
+	elem.innerHTML = "Ich bin erreichbar unter der telephone nummer: <br> 017641613749 <br> oder e-mail Adresse:<br>Hannover1992@googlemail.com" +
+		"" +
+		"" }
